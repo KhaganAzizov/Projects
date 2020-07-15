f295538b8a2b8623c08f2a08c9f3a657b340ded5
Khagan <azizovkhgn@gmail.com>
2020-07-15 22:11:38 +0400

    --developing

--- src/com/projest/ProjestBot.java
+++ src/com/projest/ProjestBot.java
@@ -16,6 +16,9 @@ import java.util.ResourceBundle;
 public class ProjestBot extends TelegramLongPollingBot {
     private final String BotToken="1228529018:AAFFONumcNaubKfghVoBM4FEZEE0azN3zyQ";
     private final String BotUsername="projest_bot";
+    String prjct_name,prjct_desc,prjct_paidornot,prjct_mail,prjct_contact;
+    boolean startedquestions=false;
+    int questnum=1;
     SendMessage smessage=new SendMessage();
     Message message=new Message();
     ResourceBundle res;
@@ -41,6 +44,22 @@ public class ProjestBot extends TelegramLongPollingBot {
                     e.printStackTrace();
                 }
             }
+            if(message.getText().equals("/go")){
+                resetasking();
+                ask_quest(questnum);
+                startedquestions=true;
+            }
+            else if(startedquestions&&questnum<=5){
+                switch (questnum){
+                    case 1->prjct_name=message.getText();
+                    case 2->prjct_desc=message.getText();
+                    case 3->prjct_paidornot=message.getText();
+                    case 4->prjct_mail=message.getText();
+                    case 5->prjct_contact=message.getText();
+                }
+                questnum++;
+                ask_quest(questnum);
+            }
         }
         else if(update.hasCallbackQuery()){
             String call_data = update.getCallbackQuery().getData();
@@ -61,6 +80,68 @@ public class ProjestBot extends TelegramLongPollingBot {
         }
     }
 
+    public void ask_quest(int questnum){
+        switch (questnum){
+            case 1->ask_prjct_name();
+            case 2->ask_prjct_desc();
+            case 3->ask_prjct_paidornot();
+            case 4->ask_mail();
+            case 5->ask_contacts();
+            case 6->prjct_announce();
+        }
+    }
+    public void ask_prjct_name(){
+        smessage.setText(res.getString("ask_prjct_name"));
+        smessage.setChatId(message.getChatId());
+        try {
+            execute(smessage);
+        }catch (TelegramApiException e){
+            e.printStackTrace();
+        }
+    }
+    public void ask_prjct_desc(){
+        smessage.setText(res.getString("ask_prjct_desc"));
+        smessage.setChatId(message.getChatId());
+        try {
+            execute(smessage);
+        }catch (TelegramApiException e){
+            e.printStackTrace();
+        }
+    }
+    public void ask_prjct_paidornot(){
+        smessage.setText(res.getString("ask_prjct_paidornot"));
+        smessage.setChatId(message.getChatId());
+        try {
+            execute(smessage);
+        }catch (TelegramApiException e){
+            e.printStackTrace();
+        }
+    }
+    public void ask_mail(){
+        smessage.setText(res.getString("ask_mail"));
+        smessage.setChatId(message.getChatId());
+        try {
+            execute(smessage);
+        }catch (TelegramApiException e){
+            e.printStackTrace();
+        }
+    }
+    public void ask_contacts(){
+        smessage.setText(res.getString("ask_contacts"));
+        smessage.setChatId(message.getChatId());
+        try {
+            execute(smessage);
+        }catch (TelegramApiException e){
+            e.printStackTrace();
+        }
+    }
+    public void prjct_announce(){
+        System.out.println("Finished");
+    }
+    private void resetasking(){
+        startedquestions=false;
+        questnum=1;
+    }
     @Override
     public String getBotUsername() {
         return BotUsername;
